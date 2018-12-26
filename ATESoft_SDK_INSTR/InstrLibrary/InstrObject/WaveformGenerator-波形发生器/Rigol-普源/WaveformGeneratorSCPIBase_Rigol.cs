6c0bce2933d0d90abca323d06c3005a0c6ba4ec2
影依丶 <779805110@qq.com>
2018-12-26 13:53:50 +0800

    增加波形信号（脉冲源）普源DG5072的多条相关指令

--- ATESoft_SDK_INSTR/InstrLibrary/InstrObject/WaveformGenerator-波形发生器/Rigol-普源/WaveformGeneratorSCPIBase_Rigol.cs
+++ ATESoft_SDK_INSTR/InstrLibrary/InstrObject/WaveformGenerator-波形发生器/Rigol-普源/WaveformGeneratorSCPIBase_Rigol.cs
@@ -10,7 +10,6 @@ namespace InstrLibrary.InstrObect
     {
         public OUTPut_System OUTPUT_SYSTEM = new OUTPut_System();
         public SOURce_System SOURCE_SYSTEM = new SOURce_System();
-        public MEMMory_System MEMMORY_SYSTEM = new MEMMory_System();
 
         /// <summary>
         /// OUTPut指令系统
@@ -28,14 +27,46 @@ namespace InstrLibrary.InstrObect
             }
         }
 
+        /// <summary>
+        /// Source指令系统
+        /// </summary>
         public class SOURce_System
         {
+            /// <summary>
+            /// Pulse脉宽设置  SOUR{0}:PULSe:WIDTh {1}
+            /// </summary>
+            /// <param name="cnum">通道号 1</param>
+            /// <param name="value">脉宽 0.5ms</param>
+            /// <returns></returns>
+            public virtual string Pulse脉宽设置(int cnum = 1, string value = "0.5ms")
+            {
+                return string.Format("SOUR{0}:PULSe:WIDTh {1}", cnum, value);
+            }
 
-        }
+            /// <summary>
+            /// Pulse占空比设置  SOUR{0}:PULSe:DCYCle {1}
+            /// </summary>
+            /// <param name="cnum">通道号 1</param>
+            /// <param name="value">value 50(%)</param>
+            /// <returns></returns>
+            public virtual string Pulse占空比设置(int cnum = 1, string value = "50")
+            {
+                return string.Format("SOUR{0}:PULSe:DCYCle {1}", cnum, value);
+            }
 
-        public class MEMMory_System
-        {
-
+            /// <summary>
+            /// Pulse频率_幅度_偏移_延时设置 SOURce{0}:APPL:PULS {1},{2},{3},{4}
+            /// </summary>
+            /// <param name="cnum">通道号 1</param>
+            /// <param name="freq">频率 1000 （= 1KHz）</param>
+            /// <param name="amp">幅度 5 （Vpp）</param>
+            /// <param name="offset">偏移 0 （vcc）</param>
+            /// <param name="delay">延时 0（ns）</param>
+            /// <returns></returns>
+            public virtual string Pulse频率_幅度_偏移_延时设置(int cnum = 1, string freq = "1000",string amp="5",string offset="0",string delay="0")
+            {
+                return string.Format("SOURce{0}:APPL:PULS {1},{2},{3},{4}", cnum, freq, amp, offset, delay);
+            }
         }
     }
 
